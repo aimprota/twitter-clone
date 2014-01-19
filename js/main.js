c3e4492b4f4c394fac4db16660c3892e074e1716
aimprota <aimprota30@gmail.com>
2014-01-18 19:57:21 -0700

    Adds hover for icon actions, and click slideToggle for stats and reply secions. Also allows user to add tweets of their own.

--- js/main.js
+++ js/main.js
@@ -23,12 +23,23 @@ $(function() {
 				button.attr("disabled", false);
 		}
 	});
-
-	$('#tweet-submit').on('click', function(){
+	var body           = $('body');
+	body.on('click', '#tweet-submit', function(){
 		var profilePic = $("#profilePic").attr("src").toString();
 		var name       = $("#name").text();
-		var newTweet = '<div class="tweet"> <div class="content"> <img class="avatar" src="'+profilePic+'" /> <strong class="fullname">'+name+'</strong> <span class="username">@mybff</span> <p class="tweet-text">Today is an amazing day.</p> <div class="tweet-actions"><ul> <li><span class="icon action-reply"></span> Reply</li> <li><span class="icon action-retweet"></span> Retweet</li> <li><span class="icon action-favorite"></span> Favorite</li> <li><span class="icon action-more"></span> More</li> </ul> </div> <div class="stats"> <div class="retweets"> <p class="num-retweets">30</p> <p>RETWEETS</p> </div> <div class="favorites"> <p class="num-favorites">6</p> <p>FAVORITES</p> </div> <div class="users-interact"> <div> <img src="img/alagoon.jpg" /> <img src="img/vklimenko.jpg" /> </div> </div> <div class="time"> 1:04 PM - 19 Sep 13 </div> </div> <div class="reply"> <img class="avatar" src="img/alagoon.jpg" /> <textarea class="tweet-compose" placeholder="Reply to @mybff"/></textarea> </div> </div> </div><!-- .tweet -->';
+		var tweet      = $(textarea).val();
+		var newTweet   = '<div class="tweet"> <div class="content"> <img class="avatar" src="'+profilePic+'" /> <strong class="fullname">'+name+'</strong> <span class="username">@'+name+'</span> <p class="tweet-text">'+tweet+'</p> <div class="tweet-actions"><ul> <li><span class="icon action-reply"></span> Reply</li> <li><span class="icon action-retweet"></span> Retweet</li> <li><span class="icon action-favorite"></span> Favorite</li> <li><span class="icon action-more"></span> More</li> </ul> </div> <div class="stats slide"> <div class="retweets"> <p class="num-retweets">30</p> <p>RETWEETS</p> </div> <div class="favorites"> <p class="num-favorites">6</p> <p>FAVORITES</p> </div> <div class="users-interact"> <div> <img src="img/alagoon.jpg" /> <img src="img/vklimenko.jpg" /> </div> </div> <div class="time"> 1:04 PM - 19 Sep 13 </div> </div> <div class="reply slide"> <img class="avatar" src="img/alagoon.jpg" /> <textarea class="tweet-compose" placeholder="Reply to @mybff"/></textarea> </div> </div> </div>';
 		$("#stream").prepend(newTweet);
+	});
+	body.on("mouseenter", ".tweet", function(){
+		$(this).find(".tweet-actions").css("visibility","visible");
+	});
+	body.on("mouseleave", ".tweet", function(){
+		$(this).find(".tweet-actions").css("visibility","hidden");
+	});
+
+	body.on("click", '.tweet', function(){
+		$(this).find(".slide").slideToggle();
 	});
 });
 
